e73b291fe884bb48943d83ee39c0eec02264e0e2
Zardimo <vasyasapeggin@rambler.ru>
2019-09-06 13:57:07 +0500

    change name functions, delete types from names, delete unusing check

--- insta_photo.py
+++ insta_photo.py
@@ -6,20 +6,20 @@ import argparse
 
 
 def create_parser():
-	parser = argparse.ArgumentParser(
-		description='This program dowloading images from any(your write) collection of hubblesite.org'
-		'or spacex for your choice')
-	parser.add_argument('-c', '--collections', action='store_true')
-	parser.add_argument('-i', '--image')
-	parser.add_argument('-u', '--upload_image', action='store_true')
-	return parser
+    parser = argparse.ArgumentParser(
+        description='This program dowloading images from any(your write) collection of hubblesite.org'
+        'or spacex for your choice')
+    parser.add_argument('-c', '--collections', action='store_true')
+    parser.add_argument('-i', '--image')
+    parser.add_argument('-u', '--upload_image', action='store_true')
+    return parser
 
 
 def dowload_image(image_name, image_url):
-    pathfile = os.path.join(os.getcwd(), 'images')
+    path = os.path.join(os.getcwd(), 'images')
     os.makedirs('images', exist_ok=True)
     response = requests.get(image_url, verify=False)
-    with open(os.path.join(pathfile, image_name), 'wb') as image:
+    with open(os.path.join(path, image_name), 'wb') as image:
         image.write(response.content)
 
 
@@ -27,25 +27,22 @@ def pull_out_prefix(image_url):
     return os.path.splitext(image_url.split('/')[-1])[1]
 
 
-def collection_image_spacex():
-    lounch_list = requests.get('https://api.spacexdata.com/v3/launches/').json()
-    all_lounch = {}
-    for lounch in lounch_list:
-        if len(lounch['links']['flickr_images'])>1:
-            all_lounch.update({lounch['mission_name'] : lounch['links']['flickr_images']})
-    return all_lounch
+def collect_image_spacex():
+    launches = requests.get('https://api.spacexdata.com/v3/launches/').json()
+    all_launch = {}
+    for launch in launches:
+        if len(launch['links']['flickr_images'])>1:
+            all_launch.update({launch['mission_name'] : launch['links']['flickr_images']})
+    return all_launch
 
-def dowload_spacex_image(all_lounch, libriary):
-    try:
-        for image_num, image_url in enumerate(all_lounch[libriary]): 
-            image_name = f'spacex_{libriary}_{image_num}{pull_out_prefix(image_url)}'
-            print(f'Download {image_name}\n.....')
-            dowload_image(image_name, image_url)
-    except KeyError:
-        exit('Неверно указана библиотека')
+def dowload_spacex_image(all_launch, libriary):
+    for image_num, image_url in enumerate(all_launch[libriary]): 
+        image_name = f'spacex_{libriary}_{image_num}{pull_out_prefix(image_url)}'
+        print(f'Download {image_name}\n.....')
+        dowload_image(image_name, image_url)
 
 
-def collection_image_hubble():
+def collect_image_hubble():
     url = 'http://hubblesite.org/api/v3/images/all'
     params = {
         'page' : 'all'
@@ -54,7 +51,7 @@ def collection_image_hubble():
     return list(set([image['collection'] for image in response.json()]))
 
 
-def get_collection_id_list(collection):
+def get_collection_ids(collection):
     url_id = 'http://hubblesite.org/api/v3/images/'
     response = requests.get(f'{url_id}{collection}').json()
     return list(image['id'] for image in response)
@@ -63,20 +60,22 @@ def get_collection_id_list(collection):
 def dowload_hubble_image(collection):
     url_image = 'http://hubblesite.org/api/v3/image/'
     extensions = ['.png', '.jpg', '.jpeg', 'pdf']
-    for id in get_collection_id_list(collection):
-        response = requests.get(f'{url_image}{id}').json()
+    for image_id in get_collection_ids(collection):
+        response = requests.get(f'{url_image}{Image_id}').json()
         images = list(image['file_url'] for image in response['image_files'])
         final_url = (f'http:{images[-1]}')
         prefix = pull_out_prefix(final_url)
-        image_name = f'{collection}_{id}{prefix}'
+        image_name = f'{collection}_{image_id}{prefix}'
         if os.path.splitext(image_name)[1].lower() in extensions:
             print(f'download {image_name} ....')
             dowload_image(image_name, final_url)
 
 
 def insta_bot(path, image_name):
+    login = os.getenv('INSTA_LOGIN')
+    password = os.getenv('INSTA_PASSWORD')
     bot = Bot()
-    bot.login(username='insta_bot_spacex_hubble', password='123321csk', proxy=None)
+    bot.login(username=login, password=password, proxy=None)
     write_name = os.path.splitext(image_name)[0]
     bot.upload_photo(path, write_name)
 
@@ -96,18 +95,18 @@ if __name__ == '__main__':
     parser = create_parser()
     args_namespace = parser.parse_args()
     if args_namespace.collections:
-    	print('hubble collections:')
-    	for collection in collection_image_hubble():
-    		print(collection)
-    	print('\nspacex libriary:')
-    	for libriary in collection_image_spacex().keys():
-    		print(libriary)
-    elif args_namespace.image in collection_image_spacex():
-        dowload_spacex_image(collection_image_spacex(), args_namespace.image)
-    elif args_namespace.image in collection_image_hubble():
+        print('hubble collections:')
+        for collection in collect_image_hubble():
+            print(collection)
+        print('\nspacex libriary:')
+        for libriary in collect_image_spacex().keys():
+            print(libriary)
+    elif args_namespace.image in collect_image_spacex():
+        dowload_spacex_image(collect_image_spacex(), args_namespace.image)
+    elif args_namespace.image in collect_image_hubble():
         dowload_hubble_image(args_namespace.image)
     elif args_namespace.upload_image:
-    	for image in  get_image_list_from_path(path):
-        	insta_bot(os.path.join(path, image), image)
+        for image in  get_image_list_from_path(path):
+            insta_bot(os.path.join(path, image), image)
     else:
-    	print('Incorrect collection')
+        print('Incorrect collection')
